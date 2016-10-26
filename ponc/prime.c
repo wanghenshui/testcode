7586a4c126c0c948aa938f21c60c760aac217c8c
jieyaren <jieyaren@gmail.com>
2016-10-26 19:43:46 +0800

    ...

--- ponc/prime.c
+++ ponc/prime.c
@@ -12,12 +12,25 @@ int main()
 	memset(loca,0,sizeof(loca));
 	
 	for(i=2; i<=100; i++)
-		for(j=2; j<i; j++)
-			if(i%j==0) loca[i] = !loca[i];
-	
+	{
+		for(j=2; j<i; j++) //这里改成j<= sqrt(double(i));更佳，因为是对称的
+		{
+			if(i%j != 0) 
+				continue;
+			//如果没除开就继续除
+			else 
+				loca[i] = !loca[i];
+				break;
+			//除开了，说明不是质数是素数，记录下来，反着打印就可以了
+		}
+		//大圈继续算,这个continue加不加都可以。
+		continue;
+
+	}
+
 
 	for(i=2; i<101; i++)
-		if(loca[i]) printf("%d ",i);
+		if(!loca[i]) printf("%d ",i);
 
 	printf("\n");
 

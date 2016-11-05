8847ded3454e16838d907f86fc8ac20fee1ee789
jieyaren <jieyaren@gmail.com>
2016-11-05 20:31:38 +0800

    lazy_at_home

---
+++ punkc/fibo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+long long int fibonacci()
+{
+	static long long int first = 0;
+	static long long int second = 1;
+	long long int out = first + second;
+
+	first = second;
+	second = out;
+	return out;
+}
+
+int main()
+{
+	for(int i =0; i<10; i++)
+		printf("%lli\n",fibonacci());
+	return 0;
+
+}

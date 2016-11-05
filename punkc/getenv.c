5a5f273f4e49b42b33ae4652733759cf196c3c5d
jieyaren <jieyaren@gmail.com>
2016-11-05 19:43:19 +0800

    lazy_at_home

---
+++ punkc/getenv.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>  //getenv, atoi
+#include <stdio.h>
+
+int main()
+{
+	char *repstext = getenv("reps");
+	int reps = repstext? atoi(repstext):10;
+
+	char *msg = getenv("msg");
+	if(!msg) msg = "Hello.";
+	for(int i=0; i<reps; i++)
+		printf("%s\n",msg);
+	return 0;
+}

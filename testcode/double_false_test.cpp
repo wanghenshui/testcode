9d70fe0e598fd20e8ae429ee09393364eda1d7d9
jieyaren <jieyaren@gmail.com>
2017-01-10 13:21:32 +0800

    ...

--- testcode/double_false_test.cpp
+++ testcode/double_false_test.cpp
@@ -3,8 +3,10 @@ int global = 8;
 
 int main()
 {
+	int tmp = !!global;
 	std::cout <<"global="<<global<<std::endl;
 	std::cout <<"!global="<<!global<<std::endl;
 	std::cout << "!!global"<<!!global<<std::endl;
+	std::cout <<"tmp="<<tmp<<std::endl;
 	return 0;
 }

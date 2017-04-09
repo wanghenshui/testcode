5c17443533b91f04ded09a9f426c58fcb579677f
jieyaren <jieyaren@gmail.com>
2017-04-09 14:24:41 +0800

    lambda

--- estl/foreach1.cpp
+++ estl/foreach1.cpp
@@ -1,17 +1,20 @@
 #include "algostuff.hpp"
 
+/*
 void print(int elem)
 {
     std::cout<<elem<<' ';
 }
-
+*/
 int main()
 {
     std::vector<int> coll;
     INSERT_ELEMENTS(coll,1,9);
 
-    std::for_each(coll.begin(),coll.end(),print);
-
+    //std::for_each(coll.begin(),coll.end(),print);
+    std::for_each(coll.begin(),
+    			  coll.end(),
+    			  [](int elem){std::cout<<elem<<' ';});//lambda
     std::cout<<std::endl;
 }
 

5fd704c7aeffcf1eaf71fde75ce3151285d85fb0
jieyaren <jieyaren@gmail.com>
2017-05-16 09:12:11 +0800

    remove

--- ciai/atom/atom.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ATOM__
-#define __ATOM__
-
-extern 		 int atom_length(const char *str);
-extern const char *atom_new(const char *str, int len);
-extern const char *atom_string(const char *str);
-extern const char *atom_int(long n);
-
-#endif

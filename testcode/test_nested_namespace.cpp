d689f15d59b0b08e6c10ef5fe849b21df4e0df4a
jieyaren <jieyaren@gmail.com>
2017-06-30 15:06:32 +0800

    test namespace

---
+++ testcode/test_nested_namespace.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include "test_nested_namespace2.hpp"
+int main(){
+
+cout << w::w::i<< " "<< w::w::y::j<<endl;
+}
---
+++ testcode/test_nested_namespace1.hpp
@@ -0,0 +1,7 @@
+namespace w{
+
+	int i=2017;
+	namespace y {
+		int j=630;
+	}
+}

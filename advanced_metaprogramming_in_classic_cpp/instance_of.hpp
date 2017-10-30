f2ccd3bbaf14b23a32536faaf87bd17914ce5c5d
wang henshui <jieyaren@gmail.com>
2017-10-30 22:47:08 +0800

    hollow types

---
+++ advanced_metaprogramming_in_classic_cpp/instance_of.hpp
@@ -0,0 +1,12 @@
+#ifndef __INSTANCE_OF
+#define __INSTANCE_OF
+template <typename T>
+struct instance_of
+{
+	typedef T type;
+	instance_of(int i=0){}
+};
+
+//usage: const instance_of<int> g = instance_of<int>()
+
+#endif

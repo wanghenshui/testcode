e0a6d888a2e2c173310ade8e340098aa5bfa5afe
wang henshui <jieyaren@gmail.com>
2017-11-05 21:35:25 +0800

    i+1 眼神不好害死人啊

--- programming_contest_challenge_book/part_sum.cpp
+++ programming_contest_challenge_book/part_sum.cpp
@@ -2,16 +2,36 @@
 #include <vector>
 using namespace std;
 int n, target;
-int a[65535];
+int a[5];
 
 bool dfs(int i, int sum)
 {
-	if (n==i)	return sum == target;
-	if (dfs(i+i, sum)) return true;
-	if (dfs(i+i, sum+a[i])) return true;
+	if (n==i)
+	{	
+		cout<< i<<'\t'<<sum<<'\n';
+		return sum == target;
+	}
+	if (dfs(i+1, sum)) 
+	{
+
+		cout<< i<<'\t'<<sum<<'\n';
+		return true;
+	}
+	if (dfs(i+1, sum+a[i])){
+
+		cout<< i<<'\t'<<sum<<'\n';
+		return true;
+	}
 	return false;
 }
 
+void solve()
+{
+	if(dfs(0,0))
+		cout<<"find it\n";
+	else
+		cout<<"fuck it\n";
+}
 int main()
 {
 	cout<<"array of num :";
@@ -21,12 +41,10 @@ int main()
 	for(int i=0; i<n; ++i)
 	{
 		cin>>a[i];
+		cout<< i<<'\t'<<a[i]<<'\n';
 	}
 	cout<<"target :";
 	cin>>target;
 
-	if(dfs(0,0))
-		cout<<"find it\n";
-	else
-		cout<<"fuck it\n";
+	solve();
 }

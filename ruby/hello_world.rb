f6af5f974d2955fcd82227aefaee84ddee86be38
wanghenshui <wanghenshui@qq.com>
2019-01-03 23:47:15 +0800

    travis test 3

--- ruby/hello_world.rb
+++ ruby/hello_world.rb
@@ -1,1 +1,14 @@
-puts "fuck world"
+#!/usr/bin/env ruby
+begin
+  gem 'minitest', '>= 5.0.0'
+  require 'minitest/autorun'
+rescue Gem::LoadError => e
+  puts 'Minitest 5.0 gem must be installed for the xRuby track.'
+end
+
+class HelloWorldTest < Minitest::Test
+	def test
+		puts "fuck world"
+		assert_equal (1,1)
+end
+

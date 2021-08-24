3bca8dd98e49e3a667b54177850a50f5a1cf90b6
Ted Mostly <wanghenshui@qq.com>
2021-08-24 16:06:29 +0800

    -lstdc++fs

---
+++ testcode/gcc7_filesystem_sucks_need_link_fs.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <experimental/filesystem>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/stat.h>
+ 
+namespace fs = std::experimental::filesystem;
+ 
+void demo_status(const fs::path& p, fs::file_status s)
+{
+    std::cout << p;
+    // alternative: switch(s.type()) { case fs::file_type::regular: ...}
+    if(fs::is_regular_file(s)) std::cout << " is a regular file\n";
+    if(fs::is_directory(s)) std::cout << " is a directory\n";
+    if(fs::is_block_file(s)) std::cout << " is a block device\n";
+    if(fs::is_character_file(s)) std::cout << " is a character device\n";
+    if(fs::is_fifo(s)) std::cout << " is a named IPC pipe\n";
+    if(fs::is_socket(s)) std::cout << " is a named IPC socket\n";
+    if(fs::is_symlink(s)) std::cout << " is a symlink\n";
+    if(!fs::exists(s)) std::cout << " does not exist\n";
+}
+ 
+int main()
+{
+    // create files of different kinds
+    fs::create_directory("sandbox");
+    std::ofstream("sandbox/file"); // create regular file
+    fs::create_directory("sandbox/dir");
+    mkfifo("sandbox/pipe", 0644);
+    sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    std::strcpy(addr.sun_path, "sandbox/sock");
+    int fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof addr);
+    fs::create_symlink("file", "sandbox/symlink");
+ 
+    // demo different status accessors
+    for(auto it = fs::directory_iterator("sandbox"); it != fs::directory_iterator(); ++it)
+        demo_status(*it, it->symlink_status()); // use cached status from directory entry
+    demo_status("/dev/null", fs::status("/dev/null")); // direct calls to status
+    demo_status("/dev/sda", fs::status("/dev/sda"));
+    demo_status("sandbox/no", fs::status("/sandbox/no"));
+ 
+    // cleanup
+    close(fd);
+    fs::remove_all("sandbox");
+}
